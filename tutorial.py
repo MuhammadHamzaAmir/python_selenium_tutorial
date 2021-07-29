1e4b8ba55e5591c2d9d69d5cf096629550889282
Muhammad Hamza Amir <muhammadhamza2084@gmail.com>
2021-07-29 14:17:36 +0500

    basic tutuoral completed\

--- tutorial.py
+++ tutorial.py
@@ -1,5 +1,11 @@
 from selenium import webdriver
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.support.ui import WebDriverWait
+import time
 
 browser = webdriver.Chrome(executable_path="../chromedriver_win32/chromedriver.exe")
 
 browser.get('https://duckduckgo.com')
+time.sleep(5)
+
+browser.quit()
