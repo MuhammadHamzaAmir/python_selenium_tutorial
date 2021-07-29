f77b6ab1dab17fffba1faf15e9bdda3ba79e8fb6
Muhammad Hamza Amir <muhammadhamza2084@gmail.com>
2021-07-29 19:02:33 +0500

    Account ifo change happens

--- tutorial.py
+++ tutorial.py
@@ -22,7 +22,7 @@ time.sleep(3)
 xpath_login_b_happen = "//*[@id='_next']/div/div/form/div[4]/button"; #xpath of login button on sign up page
 login_b_happen = driver.find_element_by_xpath(xpath_login_b_happen)
 #print(login_b_happen.rect) #{'height': 65, 'width': 270, 'x': 629, 'y': 838}
-driver.execute_script("arguments[0].scrollIntoView();", login_b_happen)
+driver.execute_script("arguments[0].scrollIntoView({behavior: 'smooth',block: 'center',inline: 'center'});", login_b_happen)
 time.sleep(2)
 
 xpath_email_if = "//*[@id='email']"; #xpath of email input field on login page
@@ -43,6 +43,59 @@ print(password.get_attribute('value'), " || ", email.get_property("value"), " ",
 login_b_happen.click()
 
 time.sleep(8)
+
+xpath_arrow_drop_down = "//*[@id='_next']/div/div[1]/div/div[1]/div/div/div/div/div/div[1]/div"; #xpath of dropdown
+xpath_menu_drop_down = "//*[@id='_next']/div/div[1]/div/div[1]/div/div/div/div/div/div[2]/ul/a"; #xpath of dropdown_menu
+
+arrow_drop_down = driver.find_element_by_xpath(xpath_arrow_drop_down)
+arrow_drop_down.click()
+time.sleep(3)
+
+menu_drop_down = driver.find_element_by_xpath(xpath_menu_drop_down)
+#menu_drop_down.click()
+driver.execute_script("arguments[0].click();", menu_drop_down)
+time.sleep(3)
+
+xpath_account = "//*[@id='_next']/div/div/div[2]/div/div[2]/div[1]/ul/li[2]/a"; #xpath of account section
+xpath_update_b = "//*[@id='_next']/div/div/div[2]/div/div/div[2]/div[2]/div/div/form[1]/div/div[2]/button"; ##xpath of update button
+xpath_voorname_first_input_field = "//*[@id='_next']/div/div/div[2]/div/div/div[2]/div[2]/div/div/form[1]/div/div[1]/div[1]/input"; #xpath of voorname first input field
+xpath_voorname_second_input_field = "//*[@id='_next']/div/div/div[2]/div/div/div[2]/div[2]/div/div/form[1]/div/div[1]/div[3]/input"; #xpath of voorname second input field
+xpath_Achternaam_input_field = "//*[@id='_next']/div/div/div[2]/div/div/div[2]/div[2]/div/div/form[1]/div/div[1]/div[2]/input"; #xpath of Achternaam input field
+
+account_b = driver.find_element_by_xpath(xpath_account)
+account_b.click()
+
+time.sleep(2.5)
+
+update_b = driver.find_element_by_xpath(xpath_update_b)
+driver.execute_script("arguments[0].scrollIntoView({behavior: 'smooth',block: 'end',inline: 'nearest'})", update_b)
+
+time.sleep(3)
+
+voorname_first_input_field = driver.find_element_by_xpath(xpath_voorname_first_input_field)
+Achternaam_input_field = driver.find_element_by_xpath(xpath_Achternaam_input_field)
+voorname_second_input_field = driver.find_element_by_xpath(xpath_voorname_second_input_field)
+
+voorname_first_input_field.clear()
+time.sleep(1)
+voorname_first_input_field.send_keys("SETT TESTER")
+
+Achternaam_input_field.clear()
+time.sleep(1)
+Achternaam_input_field.send_keys("TEST CORE RYZEN")
+
+voorname_second_input_field.clear()
+time.sleep(1)
+voorname_second_input_field.send_keys("JUST TESTING")
+
+update_b.click()
+
+time.sleep(5)
+
+driver.back()
+
+time.sleep(3)
+
 
 
 driver.quit()
